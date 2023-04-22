9640b62bd0d9f200a86b3aac17c2b848b11aa824
Isaman sangbumrung <38631981+tao-Isaman@users.noreply.github.com>
2023-04-22 09:38:49 +0000

    upated

--- 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/extract.py
+++ 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/extract.py
@@ -1,11 +1,23 @@
 import json
 
+import subprocess
+
 import requests
 
 
 if __name__ == "__main__":
-    url = ""
+    url = "https://dog.ceo/api/breeds/image/random"
     response = requests.get(url)
     data = response.json()
 
+    print(data)
+
+    test = {
+        "สวัสดี" : "ODDS"
+    }
+
+    with open("dogs.json", "a") as files : 
+        json.dump(data, files)
+
+    subprocess.run("./load.sh")    
     # Your code here
--- 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/load.sh
+++ 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/load.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -XPOST -H "Content-type: application/json" -d @dogs.json "https://getpantry.cloud/apiv1/pantry/<YOUR-PANTRY-ID>/basket/randomDogs"
+curl -XPOST -H "Content-type: application/json" -d @dogs.json "https://getpantry.cloud/apiv1/pantry/9a526925-9ee8-44da-9e8f-b3e321115f86/basket/randomDogs"
