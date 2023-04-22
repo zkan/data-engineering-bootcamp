68a3e48125b865404a1d084945531902e4672044
BoonthichaSM <43932212+BoonthichaSaejia@users.noreply.github.com>
2023-04-22 10:07:16 +0000

    Pending changes exported from your codespace

--- 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/extract.py
+++ 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/extract.py
@@ -4,8 +4,14 @@ import requests
 
 
 if __name__ == "__main__":
-    url = ""
-    response = requests.get(url)
+    url = "https://dog.ceo/api/breeds/image/random" #add any API url your want to get data
+    #requests.get = to get data
+    #requests.post= to send data
+    response = requests.get(url) 
     data = response.json()
 
+
     # Your code here
+    print(data)
+    with open("dogs.json","w") as f:
+        json.dump(data,f)
