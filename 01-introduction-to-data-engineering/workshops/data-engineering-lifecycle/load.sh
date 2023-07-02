8ac0dbb3dc2e46d8350d998e0d6bc316da002774
zkan <zkan.cs@gmail.com>
2023-07-02 12:20:48 +0700

    feat: Add load data to JSONBin.io script

--- 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/load.sh
+++ 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/load.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-curl -XPOST -H "Content-type: application/json" -d @dogs.json "https://getpantry.cloud/apiv1/pantry/<YOUR-PANTRY-ID>/basket/randomDogs"
+API_KEY='YOUR_API_KEY'
+COLLECTION_ID='YOUR_COLLECTION_ID'
+
+curl -XPOST \
+    -H "Content-type: application/json" \
+    -H "X-Master-Key: $API_KEY" \
+    -H "X-Collection-Id: $COLLECTION_ID" \
+    -d @dogs.json \
+    "https://api.jsonbin.io/v3/b"
