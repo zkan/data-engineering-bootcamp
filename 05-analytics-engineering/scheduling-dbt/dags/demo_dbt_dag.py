09f8ef20b30ec049be027bf34f00422b59a08600
lifnaja <alif@odds.team>
2024-03-07 07:24:33 +0700

    use cosmos ProfileConfig instead of conn_id in DbtDag

--- 05-analytics-engineering/scheduling-dbt/dags/demo_dbt_dag.py
+++ 05-analytics-engineering/scheduling-dbt/dags/demo_dbt_dag.py
@@ -1,17 +1,32 @@
 from airflow.utils import timezone
 
-from cosmos.providers.dbt import DbtDag
+from cosmos import DbtDag, ProjectConfig, ProfileConfig
+from cosmos.profiles import GoogleCloudServiceAccountDictProfileMapping
 
+"""
+#### Connections
+1. **bigquery_dbt**
+    [
+        conn_type=`Google Cloud`,
+        keyfile_json=`YOUR_SERVICE_ACCOUNT_JSON`,
+        project_id=`YOUR_PROJECT_ID`
+    ]
+"""
+
+profile_config = ProfileConfig(
+    profile_name="example_dbt_project",
+    target_name="dev",
+    profile_mapping=GoogleCloudServiceAccountDictProfileMapping(
+        conn_id="bigquery_dbt",
+        profile_args={"schema": "dataset_output"},
+    ),
+)
 
 example_dbt_project = DbtDag(
     dag_id="demo_dbt_dag",
     schedule_interval="@daily",
     start_date=timezone.datetime(2022, 11, 27),
-    conn_id="example_bigquery",
     catchup=False,
-    dbt_project_name="example_dbt_project",
-    dbt_args={
-        "schema": "dbt_zkan"
-    },
-    dbt_root_path="/opt/airflow/dbt",
+    project_config=ProjectConfig("/opt/airflow/dbt/example_dbt_project"),
+    profile_config=profile_config
 )
