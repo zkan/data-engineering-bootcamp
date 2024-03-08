95211e6b87754fe2d8ecaa6c2c282dd232b3fabf
Kan Ouivirach <zkan.cs@gmail.com>
2024-03-08 15:29:47 +0000

    Finish Day 2 - Loading data to BigQuery

--- 00-bootcamp-project-solution/load_data_to_bigquery.py
+++ 00-bootcamp-project-solution/load_data_to_bigquery.py
@@ -38,31 +38,87 @@ with open(file_path, "rb") as f:
 table = client.get_table(table_id)
 print(f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {table_id}")
 
+# Products
+data = "products"
+file_path = f"{DATA_FOLDER}/{data}.csv"
+with open(file_path, "rb") as f:
+    table_id = f"{project_id}.deb_bootcamp.{data}"
+    job = client.load_table_from_file(f, table_id, job_config=job_config)
+    job.result()
+
+table = client.get_table(table_id)
+print(f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {table_id}")
+
+# Order Items
+data = "order_items"
+file_path = f"{DATA_FOLDER}/{data}.csv"
+with open(file_path, "rb") as f:
+    table_id = f"{project_id}.deb_bootcamp.{data}"
+    job = client.load_table_from_file(f, table_id, job_config=job_config)
+    job.result()
+
+table = client.get_table(table_id)
+print(f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {table_id}")
+
+# Promos
+data = "promos"
+file_path = f"{DATA_FOLDER}/{data}.csv"
+with open(file_path, "rb") as f:
+    table_id = f"{project_id}.deb_bootcamp.{data}"
+    job = client.load_table_from_file(f, table_id, job_config=job_config)
+    job.result()
+
+table = client.get_table(table_id)
+print(f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {table_id}")
+
 # ----------
 
 # Events
-# job_config = bigquery.LoadJobConfig(
-#     skip_leading_rows=1,
-#     write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
-#     source_format=bigquery.SourceFormat.CSV,
-#     autodetect=True,
-#     time_partitioning=bigquery.TimePartitioning(
-#         type_=bigquery.TimePartitioningType.DAY,
-#         field="created_at",
-#     ),
-# )
+job_config = bigquery.LoadJobConfig(
+    skip_leading_rows=1,
+    write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+    source_format=bigquery.SourceFormat.CSV,
+    autodetect=True,
+    time_partitioning=bigquery.TimePartitioning(
+        type_=bigquery.TimePartitioningType.DAY,
+        field="created_at",
+    ),
+)
 
-# dt = "2021-02-10"
-# partition = dt.replace("-", "")
-# data = "events"
-# file_path = f"{DATA_FOLDER}/{data}.csv"
-# with open(file_path, "rb") as f:
-#     table_id = f"{project_id}.deb_bootcamp.{data}${partition}"
-#     job = client.load_table_from_file(f, table_id, job_config=job_config)
-#     job.result()
+dt = "2021-02-10"
+partition = dt.replace("-", "") # 20210210
+data = "events"
+file_path = f"{DATA_FOLDER}/{data}.csv"
+with open(file_path, "rb") as f:
+    table_id = f"{project_id}.deb_bootcamp.{data}${partition}"
+    job = client.load_table_from_file(f, table_id, job_config=job_config)
+    job.result()
 
-# table = client.get_table(table_id)
-# print(f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {table_id}")
+table = client.get_table(table_id)
+print(f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {table_id}")
 
-# # ถึงตรงนี้เราโหลดข้อมูลไปแล้ว 2 ชุด ยังเหลืออีก 5 ชุดที่ต้องโหลดเพิ่ม
-# # YOUR CODE HERE
+# Users
+dt = "2020-10-23"
+partition = dt.replace("-", "")
+data = "users"
+file_path = f"{DATA_FOLDER}/{data}.csv"
+with open(file_path, "rb") as f:
+    table_id = f"{project_id}.deb_bootcamp.{data}${partition}"
+    job = client.load_table_from_file(f, table_id, job_config=job_config)
+    job.result()
+
+table = client.get_table(table_id)
+print(f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {table_id}")
+
+# Orders
+dt = "2021-02-10"
+partition = dt.replace("-", "")
+data = "orders"
+file_path = f"{DATA_FOLDER}/{data}.csv"
+with open(file_path, "rb") as f:
+    table_id = f"{project_id}.deb_bootcamp.{data}${partition}"
+    job = client.load_table_from_file(f, table_id, job_config=job_config)
+    job.result()
+
+table = client.get_table(table_id)
+print(f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {table_id}")
