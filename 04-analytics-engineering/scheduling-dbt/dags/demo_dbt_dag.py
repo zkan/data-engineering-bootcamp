6afa538c54ec4bb91daeb54c6b7ea843e0a71e40
zkan <zkan.cs@gmail.com>
2024-08-10 12:58:23 +0700

    Revise to use Postgres instead of BigQuery (to make it simpler)

--- 04-analytics-engineering/scheduling-dbt/dags/demo_dbt_dag.py
+++ 04-analytics-engineering/scheduling-dbt/dags/demo_dbt_dag.py
@@ -1,32 +1,56 @@
 from airflow.utils import timezone
 
 from cosmos import DbtDag, ProjectConfig, ProfileConfig
-from cosmos.profiles import GoogleCloudServiceAccountDictProfileMapping
+# from cosmos.profiles import GoogleCloudServiceAccountDictProfileMapping
+from cosmos.profiles import PostgresUserPasswordProfileMapping
 
 """
-#### Connections
-1. **bigquery_dbt**
-    [
-        conn_type=`Google Cloud`,
-        keyfile_json=`YOUR_SERVICE_ACCOUNT_JSON`,
-        project_id=`YOUR_PROJECT_ID`
-    ]
+# Connections
+
+## BigQuery
+
+Connection Id: bigquery_dbt
+Connection Type: Google Cloud
+Project Id: YOUR_PROJECT_ID
+Keyfile JSON: YOUR_SERVICE_ACCOUNT_JSON
+
+## Postgres
+
+Connection Id: postgres_dbt
+Connection Type: Postgres
+Host: postgres
+Database: airflow
+Login: airflow
+Password: airflow
+Port 5432
 """
+
+DBT_PROJECT_DIR = "/opt/airflow/dbt/example_dbt_project"
+
+# profile_config = ProfileConfig(
+#     profile_name="example_dbt_project",
+#     target_name="dev",
+#     profile_mapping=GoogleCloudServiceAccountDictProfileMapping(
+#         conn_id="bigquery_dbt",
+#         profile_args={"schema": "dataset_output"},
+#     ),
+# )
 
 profile_config = ProfileConfig(
     profile_name="example_dbt_project",
     target_name="dev",
-    profile_mapping=GoogleCloudServiceAccountDictProfileMapping(
-        conn_id="bigquery_dbt",
+    profile_mapping=PostgresUserPasswordProfileMapping(
+        conn_id="postgres_dbt",
         profile_args={"schema": "dataset_output"},
     ),
 )
 
 example_dbt_project = DbtDag(
     dag_id="demo_dbt_dag",
-    schedule_interval="@daily",
+    schedule_interval=None,
     start_date=timezone.datetime(2022, 11, 27),
     catchup=False,
-    project_config=ProjectConfig("/opt/airflow/dbt/example_dbt_project"),
-    profile_config=profile_config
+    project_config=ProjectConfig(DBT_PROJECT_DIR),
+    profile_config=profile_config,
+    tags=["demo"],
 )
