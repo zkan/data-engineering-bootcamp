aa153f191cc88d8d3502e3a4bbcacb6fb15509e1
Alif Ruksaithong <alif@odds.team>
2024-10-04 06:55:21 +0000

    replace buckkey name

---
+++ 04-automated-data-pipelines/pyspark/transform_products.py
@@ -0,0 +1,43 @@
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructField, StructType, StringType
+
+
+
+BUSINESS_DOMAIN = "greenery"
+BUCKET_NAME = "deb-bootcamp-YOUR_STUDENT_ID"
+DATA = "products"
+KEYFILE_PATH = "/opt/spark/pyspark/_gcs.json"
+
+execution_date = os.getenv('EXECUTION_DATE')
+
+spark = SparkSession.builder.appName("greenery") \
+    .config("spark.memory.offHeap.enabled", "true") \
+    .config("spark.memory.offHeap.size", "5g") \
+    .config("fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem") \
+    .config("google.cloud.auth.service.account.enable", "true") \
+    .config("google.cloud.auth.service.account.json.keyfile", KEYFILE_PATH) \
+    .getOrCreate()
+
+struct_schema = StructType([
+    StructField("product_id", StringType()),
+    StructField("name", StringType()),
+    StructField("price", StringType()),
+    StructField("inventory", StringType()),
+])
+
+GCS_FILE_PATH = f"gs://{BUCKET_NAME}/raw/{BUSINESS_DOMAIN}/{DATA}/{execution_date}/{DATA}.csv"
+df = spark.read.option("header", True).schema(struct_schema).csv(GCS_FILE_PATH)
+df.show()
+
+df.createOrReplaceTempView("PRODUCTS_TABLE")
+result = spark.sql("""
+    select
+        *
+    from PRODUCTS_TABLE
+""")
+
+
+OUTPUT_PATH = f"gs://{BUCKET_NAME}/cleaned/{BUSINESS_DOMAIN}/{DATA}/{execution_date}/"
+
+result.write.mode("overwrite").parquet(OUTPUT_PATH)
