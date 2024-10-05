ffd97e5251222957de876875d8abe5d425b3997d
Alif Ruksaithong <alif@odds.team>
2024-10-05 08:27:30 +0700

    Bootcamp project day4 (#18)
    * Setup task extract data, load to gsc, and load to bigquey
    
    * replace buckkey name
    
    * add dags etl greenery data
    
    * set placeholder KEYFILE_PATH

---
+++ 04-automated-data-pipelines/pyspark/transform_orders.py
@@ -0,0 +1,52 @@
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructField, StructType, StringType, TimestampType
+
+
+
+BUSINESS_DOMAIN = "greenery"
+BUCKET_NAME = "deb-bootcamp-YOUR_STUDENT_ID"
+DATA = "orders"
+KEYFILE_PATH = "YOUR_KEY_FILE_PATH"
+
+execution_date = os.getenv('EXECUTION_DATE')
+
+spark = SparkSession.builder.appName("greenery") \
+    .config("spark.memory.offHeap.enabled", "true") \
+    .config("spark.memory.offHeap.size", "5g") \
+    .config("fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem") \
+    .config("google.cloud.auth.service.account.enable", "true") \
+    .config("google.cloud.auth.service.account.json.keyfile", KEYFILE_PATH) \
+    .getOrCreate()
+
+struct_schema = StructType([
+    StructField("order_id", StringType()),
+    StructField("created_at", TimestampType()),
+    StructField("order_cost", StringType()),
+    StructField("shipping_cost", StringType()),
+    StructField("order_total", StringType()),
+    StructField("tracking_id", StringType()),
+    StructField("shipping_service", StringType()),
+    StructField("estimated_delivery_at", TimestampType()),
+    StructField("delivered_at", TimestampType()),
+    StructField("status", StringType()),
+    StructField("user", StringType()),
+    StructField("promo", StringType()),
+    StructField("address", StringType()),
+])
+
+GCS_FILE_PATH = f"gs://{BUCKET_NAME}/raw/{BUSINESS_DOMAIN}/{DATA}/{execution_date}/{DATA}.csv"
+df = spark.read.option("header", True).schema(struct_schema).csv(GCS_FILE_PATH)
+df.show()
+
+df.createOrReplaceTempView("ORDERS_TABLE")
+result = spark.sql("""
+    select
+        *
+    from ORDERS_TABLE
+""")
+
+
+OUTPUT_PATH = f"gs://{BUCKET_NAME}/cleaned/{BUSINESS_DOMAIN}/{DATA}/{execution_date}/"
+
+result.write.mode("overwrite").parquet(OUTPUT_PATH)
