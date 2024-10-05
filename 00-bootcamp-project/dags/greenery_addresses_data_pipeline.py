c3d89be52151da13a2f26505e4435cb69013e08c
zkan <zkan.cs@gmail.com>
2024-10-05 08:43:43 +0700

    Move from 04 to 00

--- 00-bootcamp-project/dags/greenery_addresses_data_pipeline.py
+++ 00-bootcamp-project/dags/greenery_addresses_data_pipeline.py
@@ -12,19 +12,20 @@ from google.cloud import bigquery, storage
 from google.oauth2 import service_account
 
 
+BUCKET_NAME = "deb-bootcamp-YOUR_STUDENT_ID"
 BUSINESS_DOMAIN = "greenery"
 LOCATION = "asia-southeast1"
-GCP_PROJECT_ID = "dataengineercafe"
+GCP_PROJECT_ID = "ํYOUR_GCP_PROJECT_ID"
 DAGS_FOLDER = "/opt/airflow/dags"
 DATA = "addresses"
 
 
-def _extract_data():
+def _extract_data(ds):
     url = f"http://34.87.139.82:8000/{DATA}/"
     response = requests.get(url)
     data = response.json()
 
-    with open(f"{DAGS_FOLDER}/{DATA}.csv", "w") as f:
+    with open(f"{DAGS_FOLDER}/{DATA}-{ds}.csv", "w") as f:
         writer = csv.writer(f)
         header = [
             "address_id",
@@ -45,29 +46,28 @@ def _extract_data():
             writer.writerow(data)
 
 
-def _load_data_to_gcs():
-    keyfile_gcs = f"{DAGS_FOLDER}/dataengineercafe-deb4-uploading-files-to-gcs-27269bc8f45c.json"
+def _load_data_to_gcs(ds):
+    keyfile_gcs = f"{DAGS_FOLDER}/YOUR_KEY_FILE_PATH"
     service_account_info_gcs = json.load(open(keyfile_gcs))
     credentials_gcs = service_account.Credentials.from_service_account_info(
         service_account_info_gcs
     )
 
     # Load data from Local to GCS
-    bucket_name = "deb4-bootcamp-00"
     storage_client = storage.Client(
         project=GCP_PROJECT_ID,
         credentials=credentials_gcs,
     )
-    bucket = storage_client.bucket(bucket_name)
+    bucket = storage_client.bucket(BUCKET_NAME)
 
-    file_path = f"{DAGS_FOLDER}/{DATA}.csv"
-    destination_blob_name = f"raw/{BUSINESS_DOMAIN}/{DATA}/{DATA}.csv"
+    file_path = f"{DAGS_FOLDER}/{DATA}-{ds}.csv"
+    destination_blob_name = f"raw/{BUSINESS_DOMAIN}/{DATA}/{ds}/{DATA}.csv"
     blob = bucket.blob(destination_blob_name)
     blob.upload_from_filename(file_path)
 
 
-def _load_data_from_gcs_to_bigquery():
-    keyfile_bigquery = f"{DAGS_FOLDER}/dataengineercafe-deb4-load-files-to-bigquery-secured-4af392db6641.json"
+def _load_data_from_gcs_to_bigquery(ds):
+    keyfile_bigquery = f"{DAGS_FOLDER}/YOUR_KEY_FILE_PATH"
     service_account_info_bigquery = json.load(open(keyfile_bigquery))
     credentials_bigquery = service_account.Credentials.from_service_account_info(
         service_account_info_bigquery
@@ -83,13 +83,11 @@ def _load_data_from_gcs_to_bigquery():
     job_config = bigquery.LoadJobConfig(
         write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
         source_format=bigquery.SourceFormat.PARQUET,
-        autodetect=True,
     )
 
-    bucket_name = "deb4-bootcamp-00"
-    destination_blob_name = f"processed/{BUSINESS_DOMAIN}/{DATA}/*.parquet"
+    destination_blob_name = f"cleaned/{BUSINESS_DOMAIN}/{DATA}/{ds}/*.parquet"
     job = bigquery_client.load_table_from_uri(
-        f"gs://{bucket_name}/{destination_blob_name}",
+        f"gs://{BUCKET_NAME}/{destination_blob_name}",
         table_id,
         job_config=job_config,
         location=LOCATION,
@@ -112,16 +110,17 @@ with DAG(
     tags=["DEB", "Skooldio", "greenery"],
 ):
 
-    # Extract data from Postgres, API, or SFTP
     extract_data = PythonOperator(
         task_id="extract_data",
         python_callable=_extract_data,
+        op_kwargs={"ds": "{{ ds }}"},
     )
 
     # Load data to GCS
     load_data_to_gcs = PythonOperator(
         task_id="load_data_to_gcs",
         python_callable=_load_data_to_gcs,
+        op_kwargs={"ds": "{{ ds }}"},
     )
     
     # Submit a Spark app to transform data
@@ -129,12 +128,14 @@ with DAG(
         task_id="transform_data",
         application="/opt/spark/pyspark/transform_addresses.py",
         conn_id="my_spark",
+        env_vars={'EXECUTION_DATE': '{{ ds }}'}
     )
 
     # Load data from GCS to BigQuery
     load_data_from_gcs_to_bigquery = PythonOperator(
         task_id="load_data_from_gcs_to_bigquery",
         python_callable=_load_data_from_gcs_to_bigquery,
+        op_kwargs={"ds": "{{ ds }}"},
     )
 
     # Task dependencies
