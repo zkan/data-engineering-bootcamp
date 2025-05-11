b951f41a61424217e04f5561bb8a83eb817049ec
Kan Ouivirach <zkan.cs@gmail.com>
2025-05-11 09:05:02 +0000

    Inject context to prompt + revise the func

--- 06-data-infrastructure-for-gen-ai-app/play_with_gemini.py
+++ 06-data-infrastructure-for-gen-ai-app/play_with_gemini.py
@@ -4,17 +4,17 @@ import google.genai as genai
 import numpy as np
 
 
-def ask_gemini(client, prompt: str):
+def ask_gemini(client, model: str = "gemini-2.0-flash-001", prompt: str = ""):
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
+        model=model,
         contents=prompt,
     )
     return response.text
 
 
-def get_embedding(client, text: str):
+def get_embedding(client, model: str = "gemini-embedding-exp-03-07", text: str = ""):
     result = client.models.embed_content(
-        model="gemini-embedding-exp-03-07",
+        model=model,
         contents=text,
     )
     return result.embeddings[0]
@@ -28,12 +28,38 @@ def cosine_similarity(vec1, vec2):
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
-print(ask_gemini(client, "What are the benefits of remote work?"))
 
-embedding = get_embedding(client, "Remote work allows employees to be more flexible and productive.")
+# Prompt without context
+question = "What are the benefits of remote work?"
+print(ask_gemini(client, prompt=question))
+
+print("-" * 30)
+
+# Prompt with naive context injection
+context = """
+BearCamp offers a full refund within 14 days of booking, provided no services have been consumed.
+After 14 days, a 50% refund is possible upon review. BearCamp has no remote work policy.
+"""
+
+prompt_with_context = f"""
+You are a helpful assistant. Use the following context to answer the question.
+
+Context:
+{context}
+
+Question:
+{question}
+"""
+
+print(ask_gemini(client, prompt=prompt_with_context))
+
+print("-" * 30)
+
+# Get embeddings
+embedding = get_embedding(client, text="Remote work allows employees to be more flexible and productive.")
 print(embedding.values)
 
-vec_q = get_embedding(client, "Hello").values
-vec_c = get_embedding(client, "Hey").values
+vec_q = get_embedding(client, text="Hello").values
+vec_c = get_embedding(client, text="Hey").values
 
 print("Similarity score:", cosine_similarity(vec_q, vec_c))
