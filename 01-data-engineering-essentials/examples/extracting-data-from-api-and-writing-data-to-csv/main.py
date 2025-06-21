4631f7fb5f6ae5e0acdbdf8862f8db9ec3de1e60
Kan Ouivirach <zkan.cs@gmail.com>
2025-06-21 12:57:42 +0000

    Update API endpoint to v3

--- 01-data-engineering-essentials/examples/extracting-data-from-api-and-writing-data-to-csv/main.py
+++ 01-data-engineering-essentials/examples/extracting-data-from-api-and-writing-data-to-csv/main.py
@@ -3,9 +3,15 @@ import csv
 import requests
 
 
+BEARER_TOKEN = "YOUR_BEARER_TOKEN"
+
 # Read data from API
-url = "https://api.coincap.io/v2/exchanges"
-response = requests.get(url)
+url = "https://rest.coincap.io/v3/exchanges"
+headers = {
+    "Authorization": f"Bearer {BEARER_TOKEN}",
+    "Content-Type": "application/json"
+}
+response = requests.get(url, headers=headers)
 data = response.json()["data"]
 
 # Write data to CSV
