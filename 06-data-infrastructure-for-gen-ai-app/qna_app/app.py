6c49be321e4926f3bbf08a3137184ce137f0707a
zkan <kan@odds.team>
2025-07-13 22:30:12 +0700

    Take input and print out on the page

--- 06-data-infrastructure-for-gen-ai-app/qna_app/app.py
+++ 06-data-infrastructure-for-gen-ai-app/qna_app/app.py
@@ -3,6 +3,9 @@ import streamlit as st
 
 def main():
     st.title("Q&A App")
+    user_question = st.text_input("Ask a question:")
+    if user_question:
+        st.write(user_question)
 
 
 if __name__ == "__main__":
